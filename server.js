d6d20aa3ebc1f56088b1e19720daa62f1f9cd188
truong <nhom1hd1@gmail.com>
2021-04-01 21:30:11 +0700

    update

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express')
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('HR', 'admin', 'h0cm0n21!', {
+    host: 'sambusport.cjq2ozg3e8cp.ap-southeast-1.rds.amazonaws.com',
+    dialect: 'mysql',
+    logging: (...msg) => console.log(msg),
+});
+
+
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/', async (req, res) => {
+    await sequelize.authenticate();
+    const [results, metadata] = await sequelize.query("SELECT * FROM HR.SALARYS");
+    res.json({ results, metadata })
 })
 
 app.listen(port, () => {
